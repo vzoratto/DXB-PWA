5d2cd39f717da740e001530872332bf45596844d
aluminedr <aluminede@gmail.com>
2019-05-10 17:17:20 -0300

    Update index.php

---
+++ views/Inscripcion/index.php
@@ -0,0 +1,41 @@
+<?php
+use yii\helpers\Html;
+use yii\jui\Tabs;
+$this->title = 'Formulario de inscripcion';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="inscripciones-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php echo Tabs::widget([
+    'items' => [
+        [
+            'label' => 'Datos Personales',
+            'content' => '',
+        ],
+        [
+            'label' => 'Datos de contacto',
+            'content' => '',
+
+        ],
+        [
+            'label' => 'Datos medicos',
+            'content' => '',
+        ],
+        [
+            'label' => 'Contacto de emergencia',
+            'content' => '',
+        ],
+        [
+            'label' => 'Encuesta',
+            'content' => '',
+        ],
+    ],
+    'options' => ['tag' => 'div'],
+    'itemOptions' => ['tag' => 'div'],
+    'headerOptions' => ['class' => 'my-class'],
+    'clientOptions' => ['collapsible' => false],
+]);
+
+?>
+</div>
