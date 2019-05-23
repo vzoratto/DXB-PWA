60f662a7b03430c80c0e906aaa3a83ba467e9a7f
alewachu <49759859+alewachu@users.noreply.github.com>
2019-05-23 18:02:01 -0300

    cambios nombres
    cambios de nombres

--- views/Inscripcion/encuesta.php
+++ views/Inscripcion/encuesta.php
@@ -6,6 +6,6 @@ use yii\helpers\Url;
 ?>
 
 
-<H1>Contenido en desarrollo &#128077;</H1>
+<H1>Contenido en desarrollo &#128077; ❤️</H1>
 
 <?= Html::a('Ir a generacion de encuesta', Url::toRoute('encuesta/create'), ['class'=>'btn btn-primary btn-lg'])?>
