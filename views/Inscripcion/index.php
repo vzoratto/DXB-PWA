d68228fd855647b171a3dd08b5bd0c91a20bb146
aluminedr <aluminede@gmail.com>
2019-05-29 11:40:40 -0300

    DatosPersonales

--- views/Inscripcion/index.php
+++ views/Inscripcion/index.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\jui\Tabs;
 use yii\widgets\ActiveForm;
 use buttflattery\formwizard\FormWizard;
+use kartik\date\DatePicker;
 
 
 
@@ -26,20 +27,101 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php echo FormWizard::widget([
     'formOptions' => [
         'id' => 'my_form_multi_model_single_step',
+        'enableClientValidation'=>true,
+        //'enableAjaxValidation'=>true,
         'options'=>[
             'class'=>'form-inline'
         ],
     ],
     'theme' => FormWizard::THEME_CIRCLES,
+    'enablePersistence' => true,
     'steps' => [
         [
-            'model'=>[$persona,$usuario],
+            'model'=>[$usuario,$persona],
             'title' => 'Datos personales',
             'description' => 'Paso 1',
             'formInfoText' =>'Los campos marcados con * son obligatorios',
             'fieldConfig' => [
                 'only' => ['dniCapitan','dniUsuario','nacionalidadPersona', 'nombrePersona','apellidoPersona','fechaNacPersona','sexoPersona','idTalleRemera'], // only these field will be added in the step, rest all will be hidden/ignored.
-            ]    
+                'dniCapitan' => [
+                    'labelOptions' => [
+                        'label' => 'D.N.I. Capitan'
+                    ],
+                    'options' => [
+                        'type' => 'text'
+                    ]
+                ],
+                'dniUsuario' => [
+                    'labelOptions' => [
+                        'label' => 'D.N.I.'
+                    ],
+                    'options' => [
+                        'type' => 'text'
+                    ]
+                ],
+                'nacionalidadPersona' => [
+                    'labelOptions' => [
+                        'label' => 'Nacionalidad'
+                    ],
+                    'options' => [
+                        'type' => 'text'
+                    ]
+                ],
+                'nombrePersona' => [
+                    'labelOptions' => [
+                        'label' => 'Nombre'
+                    ],
+                    'options' => [
+                        'type' => 'text'
+                    ]
+                ],
+                'apellidoPersona' => [
+                    'labelOptions' => [
+                        'label' => 'Apellido'
+                    ],
+                    'options' => [
+                        'type' => 'text'
+                    ]
+                ],
+                'fechaNacPersona' => [
+                    'labelOptions' => [
+                        'label' => 'Fecha de Nacimiento'
+                    ],
+                    'widget' => DatePicker::class, //widget class name
+                        'options' => [ // you will pass the widget options here
+                            'options' => [
+                                'type' => DatePicker::TYPE_COMPONENT_PREPEND,
+                                'class' => 'form-control',
+                                'placeholder' => 'Seleccione su fecha de nacimiento',
+                                'language' => 'es',//definicion del lenguaje del widget
+                                'pluginOptions' => [
+                                    'autoclose'=>true,
+                                    'format' => 'yyyy-mm-dd',//definicion del formato de fecha 
+                                ]
+                            ]
+                        ]
+                ],
+                'sexoPersona' => [
+                    'labelOptions' => [
+                        'label' => 'Sexo'
+                    ],
+                    'options' => [
+                        'type' => 'radio',
+                        'itemsList' => ["F" => 'Femenino', "M" => 'Masculino'], // the radio inputs to be created for the radioList
+                    ]
+                ],
+                'idTalleRemera' => [
+                    'labelOptions' => [
+                        'label' => 'Talle remera'
+                    ],
+                    'options' => [
+                        'type' => 'dropdown',
+                        'itemsList' => $listadoTalles, //the list can be from the database
+                        'prompt' => 'Seleccione su talle',
+                    ]
+                ],
+            ],
+            'fieldOrder'=>['dniUsuario','dniCapitan','nacionalidadPersona', 'nombrePersona','apellidoPersona','fechaNacPersona','sexoPersona','idTalleRemera']    
         ],
         [
             'model' =>[$personaDireccion,$persona,$provincia],
