5ac498b7aafef4d632337215a9e857168e7b97a1
Dario <dasemenzato@hotmail.com>
2019-06-02 13:15:01 -0300

    Falta de encuesta solucionada

--- views/Encuesta/_texto.php
+++ views/Encuesta/_texto.php
@@ -4,9 +4,9 @@ use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
-
 /* @var $respuesta app\models\Respuesta */
 /* @var $form yii\widgets\ActiveForm */
+
 ?>
 
 
