dd18c99c85348c02e65322715925e476b9e82267
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-06-13 18:32:43 -0300

    cambiando nombre db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=carrera2',
+    'dsn' => 'mysql:host=localhost;dbname=carrera',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
