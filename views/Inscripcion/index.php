9f9102739de77d4c6d72fe159cfadb0a72dbd6c5
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-06-24 17:22:21 -0300

    funciona casi

--- views/Inscripcion/index.php
+++ views/Inscripcion/index.php
@@ -17,6 +17,7 @@ use buttflattery\formwizard\FormWizard;
           "action"=>"index.php?r=inscripcion%2Fstore",
       		"enableClientValidation"=>true,
       ]); ?>
+        <input type="hidden" value="0" id="editar">
 
 <?php
 
