e0ac14d58976f6160d7bccedb005df80c73d4adf
lilianapaez <lilianapaez@live.com.ar>
2019-07-04 13:19:23 -0300

    control y visualizacion pago en barra ppal

--- models/Pago.php
+++ models/Pago.php
@@ -106,5 +106,23 @@ class Pago extends \yii\db\ActiveRecord
          return $query->suma;
     }
 
-    
+    public function buscaequipo(){
+        
+        $estadopago=0;//0 para los equipos que no pagaron
+        if(!Yii::$app->user->isGuest){
+        $persona=Persona::findOne(['idUsuario'=>$_SESSION['__id']]);
+        $grupo=Grupo::findOne(['idPersona'=>$persona->idPersona]);
+        if($grupo!=null){
+          $estadoequipo=Estadopagoequipo::findOne(['idEquipo'=>$grupo->idEquipo]);
+            if($estadoequipo!=null ){
+               if($estadoequipo->idEstadoPago==2){
+                   $estadopago=2; //2 para los equipos con pago parcial
+              }else{
+                $estadopago=3;//3 para los equipos pago total o cancelo
+              }
+            }   
+          }
+        }
+        return $estadopago;
+    }
 }
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -11,7 +11,8 @@ use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
 AppAsset::register($this);
-
+$pago=new \app\models\Pago();
+$estadoequipo=$pago->buscaequipo();
 ?>
 
 
@@ -49,7 +50,7 @@ AppAsset::register($this);
           ['label' => 'Reglamento', 'url' => ['/site/index', '#' => 'reglamento']],
           ['label' => 'Contacto', 'url' => ['/site/index', '#' => 'contacto']],
           ['label' => 'Iniciar Sesión', 'url' => 'index.php?r=site%2Flogin', 'visible' => Yii::$app->user->isGuest],
-          ['label' => 'Pagar inscripcion', 'url' => 'index.php?r=pago/create', 'visible' => !Yii::$app->user->isGuest],
+          ($estadoequipo==2 || $estadoequipo==0)?['label' => 'Pagar inscripcion', 'url' => 'index.php?r=pago/create', 'visible' => !Yii::$app->user->isGuest]:'',
           !Yii::$app->user->isGuest ?(
           ['label' =>"<i class='fa fa-user-circle-o ml-30 ml-sm-0' aria-hidden='true'></i>",'items' => [
             ['label' => 'Mi perfil', 'url' => 'index.php?r=editar%2Feditar'],
