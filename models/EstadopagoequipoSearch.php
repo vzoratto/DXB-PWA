1787cc23a226271962772434210a0bbdd6fdb5f0
Dario <dasemenzato@hotmail.com>
2019-07-07 12:18:08 -0300

    Arregaldo conflicto

--- models/EstadopagoequipoSearch.php
+++ models/EstadopagoequipoSearch.php
@@ -1,11 +1,6 @@
 <?php
 
 namespace app\models;
-<<<<<<< HEAD
-//estado del equipo
-=======
-
->>>>>>> parent of 874d7b6... Revert "Merge branch 'lili-ultimo'"
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\models\Estadopagoequipo;
