c920cbf4edd674830601a566b35a1c93635d35d0
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-08-02 04:49:42 -0300

    arreglando validacion manual  encuesta salud 5 preguntas

--- web/js/controlQuintoStep.js
+++ web/js/controlQuintoStep.js
@@ -24,38 +24,17 @@ function validarEncuesta(){
     siguiente = false;
     //pregunta
 
-    var pregunta1=$('input[name="5"]:checked').val();
-    var pregunta2=$('input[name="6"]:checked').val();
-    var pregunta3=$('input[name="7"]:checked').val();
-    var pregunta4=$('input[name="8"]:checked').val();
-    var pregunta5=$('input[name="9"]:checked').val();
+    var pregunta1=$('input[name="1"]:checked').val();
+    var pregunta2=$('input[name="2"]:checked').val();
+    var pregunta3=$('input[name="3"]:checked').val();
+    var pregunta4=$('input[name="4"]:checked').val();
+    var pregunta5=$('input[name="5"]:checked').val();
 
-    if(pregunta1!==undefined){
-        siguiente=true;
-    }else{
-        siguiente=false;
-    }
-
-    if(pregunta2!==undefined){
+    if(pregunta1!==undefined && pregunta2!==undefined && pregunta3!==undefined && pregunta4!==undefined && pregunta5!==undefined){
         siguiente=true;
     }else{
         siguiente=false;
     }
 
-    if(pregunta3!==undefined){
-        siguiente=true;
-    }else{
-        siguiente=false;
-    }
-    if(pregunta4!==undefined){
-        siguiente=true;
-    }else{
-        siguiente=false;
-    }
-    if(pregunta5!==undefined){
-        siguiente=true;
-    }else{
-        siguiente=false;
-    }
     return siguiente;
 }
