eaeea7d4f7c1263667a452977950fe51dc0c5ace
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-09-05 21:55:45 -0300

    permitiendo login

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -112,8 +112,8 @@ class SiteController extends Controller
      * @return Response|string
      */
     public function actionLogin(){
-        $mensaje='NO HAY MAS CUPOS, LA INSCRIPCIÓN FUE CERRADA';
-        return Yii::$app->response->redirect(['site/index', 'guardado' => false, 'mensaje' => $mensaje])->send();
+        //$mensaje='NO HAY MAS CUPOS, LA INSCRIPCIÓN FUE CERRADA';
+        //return Yii::$app->response->redirect(['site/index', 'guardado' => false, 'mensaje' => $mensaje])->send();
         if (!Yii::$app->user->isGuest) {
             return $this->goHome();
         }
